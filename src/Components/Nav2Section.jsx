679f66d753fd0cc23603b48479edc65f44161e3c
Faraz Ahmed <fa4258745@gmail.com>
2025-07-19 18:19:16 +0530

    gamingStore

--- src/Components/Nav2Section.jsx
+++ src/Components/Nav2Section.jsx
@@ -4,14 +4,16 @@ const Nav2Section = () => {
   const navigate = useNavigate();
   return (
     <>
-      
-      <div className="main-div" style={{backgroundColor:"#eaeaea",color:"#1F1F1F"}}>
+      <div
+        className="main-div"
+        style={{ backgroundColor: "#eaeaea", color: "#1F1F1F" }}
+      >
         <p
           onClick={() => {
             navigate("/deals");
           }}
         >
-          Deals 
+          Deals
         </p>
 
         <p
@@ -30,19 +32,55 @@ const Nav2Section = () => {
           Customer Support
         </p>
 
-        <p onClick={()=> {
-          navigate("/myorder")
-        }}>My Orders</p>
+        <p
+          onClick={() => {
+            navigate("/myorder");
+          }}
+        >
+          My Orders
+        </p>
 
-        <p onClick={()=> {
-          navigate("/menu")
-        }}>Menu</p>
+        <p
+          onClick={() => {
+            navigate("/menu");
+          }}
+        >
+          Menu
+        </p>
 
-        <p onClick={()=> {
-          navigate("/delivery")
-        }}>Delivery</p>
+        <p
+          onClick={() => {
+            navigate("/delivery");
+          }}
+        >
+          Delivery
+        </p>
+
+        {/* <a href="https://red-theme-react-restaurent-web-app.vercel.app/">
+  <button style={{ padding: "8px 16px", backgroundColor: "#fff", color: "#000" }}>
+    Switch to Red Theme
+  </button>
+</a> */}
+
+        <a href="https://red-theme-react-restaurent-web-app.vercel.app/">
+          <button
+            style={{
+              padding: "0px 46px",
+              width:"100%",
+              backgroundColor: "#ffffff",
+              color: "#ff0000",
+              border: "2px solid #ff0000",
+              cursor: "pointer",
+            }}
+          >
+            Red Theme
+          </button>
+        </a>
+
+  
+
+
       </div>
-
     </>
   );
 };
